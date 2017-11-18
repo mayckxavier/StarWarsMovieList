d40b154ae2e3db6d33271dc2cdf3e71c6205a063
Mayck Xavier <mayckxavier@gmail.com>
2017-11-18 04:19:17 -0200

    Creating application class

---
+++ app/src/main/java/com/mayckxavier/starwarsmovielist/StarWarsMovieList.kt
@@ -0,0 +1,13 @@
+package com.mayckxavier.starwarsmovielist
+
+import android.app.Application
+
+/**
+ * Created by mayck on 18/11/17.
+ */
+class StarWarsMovieList: Application() {
+    override fun onCreate(){
+        super.onCreate()
+        RetrofitInitializer()
+    }
+}
