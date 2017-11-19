71e4484d32925550877250a40954c8a0e6829b20
Mayck Xavier <mayckxavier@gmail.com>
2017-11-19 14:50:12 -0200

    Adding progress dialog

--- app/src/main/java/com/mayckxavier/starwarsmovielist/ui/ListActivity.kt
+++ app/src/main/java/com/mayckxavier/starwarsmovielist/ui/ListActivity.kt
@@ -1,10 +1,16 @@
 package com.mayckxavier.starwarsmovielist.ui
 
+import android.app.ProgressDialog
+import android.opengl.Visibility
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.mayckxavier.starwarsmovielist.R
 import kotlinx.android.synthetic.main.list_activity.*
+import android.widget.RelativeLayout
+
 
 class ListActivity : AppCompatActivity() {
 
@@ -18,9 +24,23 @@ class ListActivity : AppCompatActivity() {
         movieList = movie_list
         listPresenter = ListPresenter(this)
         listPresenter.loadMovies()
+
+        swiperefresh.setOnRefreshListener {
+            movieList.adapter = null
+            listPresenter.loadMovies()
+        }
     }
 
     fun updateList(arrayAdapter: FilmListAdapter) {
         movieList.adapter = arrayAdapter
+    }
+
+    fun showProgressBar() {
+        progressbar.visibility = View.VISIBLE
+    }
+
+    fun dismissProgressBar() {
+        swiperefresh.isRefreshing = false
+        progressbar.visibility = View.GONE
     }
 }
--- app/src/main/java/com/mayckxavier/starwarsmovielist/ui/ListPresenter.kt
+++ app/src/main/java/com/mayckxavier/starwarsmovielist/ui/ListPresenter.kt
@@ -16,43 +16,24 @@ class ListPresenter(val listActivity: ListActivity) {
     }
 
     fun loadMovies() {
+        listActivity.showProgressBar()
+
         val call = RetrofitInitializer().filmService().list()
         call.enqueue(object : Callback<Films> {
             override fun onFailure(call: Call<Films>?, t: Throwable?) {
                 Log.e("erro onFailure", t?.message)
+                listActivity.dismissProgressBar()
             }
-
 
             override fun onResponse(call: Call<Films>?, response: Response<Films>?) {
                 response?.body()?.let {
                     val films: Films = it
-//                    listActivity.updateList()
 
                     val arrayAdapter = FilmListAdapter(films.results, listActivity)
-//                    val arrayAdapter = ArrayAdapter(listActivity, R.layout.simple_list_item_1, films)
-
-//                    val recyclerView = note_list_recyclerview
-//                    recyclerView.adapter = NoteListAdapter(notes, this)
-//                    val layoutManager = StaggeredGridLayoutManager(
-//                            2, StaggeredGridLayoutManager.VERTICAL)
-//                    recyclerView.layoutManager = layoutManager
-
-
                     listActivity.updateList(arrayAdapter)
-
+                    listActivity.dismissProgressBar()
                 }
             }
         })
-
-
-        val movies = listOf("Star Wars III - Uma Nova Esperança",
-                "Star Wars IV - O Império Contra-ataca",
-                "Star Wars V - O Retorno de Jedi")
-
-
     }
-
-//    fun films(): List<Film> {
-//
-//    }
 }
