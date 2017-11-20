7e82c25ed3248e069a70d1a6d6b22f95a65ffb02
Mayck Xavier <mayckxavier@gmail.com>
2017-11-20 00:23:44 -0200

    Refactoring of Film class

--- app/src/main/java/com/mayckxavier/starwarsmovielist/data/Film.kt
+++ app/src/main/java/com/mayckxavier/starwarsmovielist/data/Film.kt
@@ -1,15 +1,36 @@
 package com.mayckxavier.starwarsmovielist.data
 
+import java.io.Serializable
+
 /**
- * Created by mayck on 18/11/17.
+ * Created by Mayck Xavier <contato@mayckxavier.com> on 18/11/17.
  */
 class Film(val title: String,
-           val episodeId: Int,
-           val openingCrawl: String,
+           val episode_id: Int,
+           val opening_crawl: String,
            val director: String,
-           val producer: String) {
+           val producer: String,
+           val release_date: String,
+           val url: String): Serializable {
 
     override fun toString(): String {
         return title
+    }
+
+    fun filmId(): String {
+        val splited = url.split("/")
+
+
+        // for(part in splited){
+        //     if(!part.isBlank()){
+        //		stringParts.add(part)
+        //   }
+        //}
+        //Deixado apenas como exemplo e aprendizado. O código abaixo é o equivalente ao acima
+        val stringParts: MutableList<String> = splited
+                .filterNot { it.isBlank() }
+                .toMutableList()
+
+        return (stringParts[stringParts.lastIndex])
     }
 }
